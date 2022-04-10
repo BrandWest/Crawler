9624f8f57eab3cbcd693b831c5bba59bbe4ca126
caboose <brand.jwest@gmail.com>
2022-04-10 17:40:10 -0400

    Mostly working, has issues getting into different folder structures

--- scraper.py
+++ scraper.py
@@ -11,7 +11,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import WebDriverException
-
+from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
 
 import pickle
 import time, os, sys, getopt, csv, re
@@ -22,101 +24,237 @@ def download(url, directory, driver):
 	actions = ActionChains(driver)
 
 	stack = []
-	files_readable = []
+	directory_path = []
 	driver.get(url)
 	time.sleep(10)
 	url = url + 'post/USLfL9XIAOk9xnX562344fec4aac8'
-	time.sleep(10)
+	# time.sleep(10)
 
 	driver.get(url)
 	with open (directory + "fileStructure.csv", "w") as file:
 		writer = csv.writer(file)
+		element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,"file-listing__item")))
+		file = driver.find_element(By.CLASS_NAME, "file-listing__item").text.split('\n')
+		directory_path.append(file[0]) #Root
+		stack.append(driver.find_element(By.CLASS_NAME, "file-listing__item")) #Root
+		writer.writerow([str(file[0])])
+		actions.move_to_element(element).double_click().perform()
 		while(True):
-			element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,"file-listing__item")))
-			# time.sleep(5)
-			# print(element)
-			# # print(driver)
-			# print(element.text)
-			file = driver.find_element(By.CLASS_NAME, "file-listing__item").text.split('\n')
-			files_readable.append(file[0]) #Root
-			stack.append(driver.find_element(By.CLASS_NAME, "file-listing__item")) #Root
-			writer.writerow([str(file[0])])
-			actions.move_to_element(element).double_click().perform()
-			print("download loop")
-			driver, cookies, writer, stack, files_readable = getFileStructure(element, driver, writer, stack, files_readable)
-			exit(1)
-def getFileStructure(element, driver, writer, stack, files_readable):
+			print("while loop")
+			ignored_exceptions=(NoSuchElementException,StaleElementReferenceException,)
+			element = WebDriverWait(driver, 10,ignored_exceptions=ignored_exceptions).until(EC.presence_of_element_located((By.CLASS_NAME,"file-listing__item")))
+			driver, writer, stack, directory_path = getFileStructure(element, driver, writer, stack, directory_path)
+			
+			# your_element = WebDriverWait(your_driver, some_timeout,ignored_exceptions=ignored_exceptions).until(expected_conditions.presence_of_element_located((By.ID, my_element_id)))
+
+
+def getFileListing(elements, driver, writer, stack, directory_path):
+	elements = driver.find_elements(By.CLASS_NAME,"file-listing__item")
+	for i in elements:
+		directory_path.append(i.get_attribute('data-dir'))
+		stack.append(i)
+		writer.writerow(str([i.get_attribute('data-dir')]))
+
+	return stack, directory_path
+
+#Check if its a folder, or not.
+def folder_nav(stack, directory_path, driver):
 	actions = ActionChains(driver)
-	#get cookies
+
+	for element in stack[::-1]:
+		print("folder nav", element.text)
+		first_in = stack.pop()
+		cookies = get_cookies(driver)
+
+		actions.move_to_element(first_in).double_click().perform()
+
+		return driver
+
+	if 'folder' in on_end.get_attribute('outerHTML'):
+		final_page = 'folder'
+	else:
+		final_page = 'no_folder'
+
+	return final_page, back_one_page
+
+def get_cookies(driver):
 	pickle.dump(driver.get_cookies(), open("cookies.pkl","wb"))
+	cookies = pickle.load(open("cookies.pkl", "rb"))
+	
+	#iterate the cookies
+	for cookie in cookies:
+		driver.add_cookie(cookie)
+
+	return driver
+
+def check_paging(driver):
+	#check to see if paging is required
+	forward = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,"on-end")))
+	pagination = driver.find_element(By.CLASS_NAME, 'file-listing__pagination')
+	on_end = pagination.find_element(By.CLASS_NAME, "on-end")
+	back = pagination.find_element(By.CLASS_NAME, "back-one-page")
+
+	# <button class="on-start disabled"></button>
+	
+	if 'disabled' in on_end.get_attribute('outerHTML'):
+		final_page = 'disabled'
+	else:
+		final_page = 'active'
+
+	if 'disabled' in back.get_attribute('outerHTML'):
+		back_one_page = 'disabled'
+	else:
+		back_one_page = 'active'
+
+	return final_page, back_one_page
+
+def getFileStructure(element, driver, writer, stack, directory_path):
+	actions = ActionChains(driver)
+	print("get File structure")
+	#get cookies
+	driver = get_cookies(driver)
 
 	time.sleep(2)
-	try:
-		#wait for populate
-		element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "file-listing__item")))
-	finally:
-		#get all folders/files
-		elements = driver.find_elements(By.CLASS_NAME,"file-listing__item")
-		#apply each file to stack
-		for i in elements:
-			# print("element:", i.text)
-			file = i.text.split('\n')
-			files_readable.append(file[0])
-			stack.append(i)
-			writer.writerow([str(file[0])])
+	# driver.navigate().refresh()
+	#wait for populate
+	element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "file-listing__item")))
+	
+	#get all folders/files
+	elements = driver.find_elements(By.CLASS_NAME,"file-listing__item")
+	forward_pages, backward_pages = check_paging(driver)
+	stack_printing(stack, directory_path)
 
-		# print('-------------------------------------------')
-		# print("The Stack: ", stack, files_readable)
-		print('-------------------------------------------')
-		print("The readable: ", files_readable)
-		print('-------------------------------------------')
-		cookies = pickle.load(open("cookies.pkl", "rb"))
-		# print(cookies)
-		for cookie in cookies:
-			driver.add_cookie(cookie)
+
+
+	if forward_pages == 'active':
+		print("click required")
+		forward_flag = 1
 		
-	#check to see if paging is required
-	try:
-		# driver.find_element(By.CLASS_NAME,"file-listing__pagination")
-		# element = driver.find_element(By.CLASS_NAME,"on-end")
-		element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "on-end")))
+		#go to end of pages
+		time.sleep(2)
+		actions.move_to_element(driver.find_element(By.CLASS_NAME, 'on-end')).click().perform()
+		# element = driver.find_element(By.CLASS_NAME, 'on-end')
 		# print(element.text)
-		print("Try to click on-end")
-		if element.text == "disabled":
-			print("Disabled", element.text)
-			for first_out in files_readable[::-1]:
-				first_out_element = stack.pop()
-				if re.search(r'\.*', first_out):
-					value = files_readable.pop()
-					writer.writerow([str(value)])
-				else:
-					#Go into the folder.
-					# element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "file-listing__item")))
-					actions.move_to_element(first_out_element).click().perform()
-					getFileList(element, driver, writer, stack, files_readable)
+		time.sleep(2)
+		# element.click().perform()
+		driver.find_elements(By.CLASS_NAME, "file-listing__item")
+		time.sleep(2)
+		stack, directory_path = getFileListing(elements, driver, writer, stack, directory_path)
+		stack_printing(stack, directory_path)
+		forward_pages, backward_pages = check_paging(driver)
 
-			for i in elements[::-1]:
-				# print("element i: ", i)
-				actions.move_to_element(element).double_click().perform()
-				driver, cookies, writer, stack, files_readable = getFileList(element, driver, cookies, writer, stack, files_readable)
+		# stack, directory_path = getFileListing(element, driver, writer, stack, directory_path)
+		# folder_nav(stack, directory_path, driver)
 
-				stack.pop()
-		else:
-			print("enabled click", element.text)
-			actions.move_to_element(element).click().perform()
-	except TimeoutException as e:
-		element = -1
-		print("TimeoutException",element.text)
-	except WebDriverException:
-		element = -1
-		print("No futher pages",element.text)
-	finally:
-		if len(stack) == 0:
-			return driver, cookies, writer, stack, files_readable
-		else:
-			getFileStructure(element, driver, writer, stack, files_readable)
 
-def getFileList(element, driver, cookies, writer, stack, files_readable):
+		while backward_pages == 'active':
+			print("back one page")
+			print("going into folder") 
+			driver = folder_nav(stack, directory_path, driver)
+			time.sleep(5)
+			# backward = driver.find_element(By.CLASS_NAME, 'back-one-page')
+			ignored_exceptions=(NoSuchElementException,StaleElementReferenceException,)
+			backward = WebDriverWait(driver, 10, ignored_exceptions=ignored_exceptions).until(EC.presence_of_element_located((By.CLASS_NAME,"back-one-page")))
+
+			if backward_pages == 'active' and forward_flag == 1:
+				stack, directory_path = getFileListing(element, driver, writer, stack, directory_path)
+
+
+				print("backward click")
+				backward = WebDriverWait(driver, 10, ignored_exceptions=ignored_exceptions).until(EC.presence_of_element_located((By.CLASS_NAME,"back-one-page")))
+				backward = driver.find_element(By.CLASS_NAME, 'back-one-page')
+				# print("backward, pass: ", backward.get_attribute('back-one-page'))
+				actions.move_to_element(backward).click().perform()
+				stack, directory_path = getFileListing(element, driver, writer, stack, directory_path)
+
+			else:
+				print("No backward click, return.")
+				back_one_dir = WebDriverWait(driver,10).until(EC.presence_of_element_located(By.CLASS_NAME,'file__back active'))
+				back_one_dir = driver.find_element(By.CLASS_NAME,'file__back')
+				actions.move_to_element(back_one_dir).click()
+				getFileStructure(element, driver, writer, stack, directory_path)
+			
+			forward_pages, backward_pages = check_paging(driver)
+
+
+
+			#Check the stack, see if the item is there. If there, and back-on-page is disabled. Click return
+			#Get all file listings, go through each.
+				# Root -> Y -> User1 -> Folder1 -> item
+
+
+	elif forward_pages == 'disabled':
+		print("NO click required")
+		stack, directory_path = getFileListing(elements, driver, writer, stack, directory_path)
+	
+
+		for first_out in directory_path[::-1]:
+			first_out_element = stack.pop()
+			print("First Out:", first_out)
+			print("First Out Element:",first_out_element)
+			#If it is a file, write the value and pop
+
+			
+
+			# Here do the folder vs the regex
+			element = first_out_element.get_attribute("outerHTML")
+			if 'file__name--folder' not in element:
+				print("NOT a folder")
+				directory_path_value = directory_path.pop()
+				# writer.writerow([str(directory_path_value)])
+			else:
+				print("a folder")
+				actions.move_to_element(first_out_element).double_click().perform()
+	
+				#if not a file, Go into the folder.
+				# element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "file-listing__item")))
+				getFileStructure(element, driver, writer, stack, directory_path)
+
+		'''
+
+
+
+
+		'''				
+		# driver, writer, stack, directory_path = getFileList(element, driver, writer, stack, directory_path)
+
+		# for i in elements[::-1]:
+		# 	# print("element i: ", i)
+		# 	actions.move_to_element(element).double_click().perform()
+		# 	getFileStructure(element, driver, writer, stack, directory_path)
+		# 	# driver, writer, stack, directory_path = getFileList(element, driver, writer, stack, directory_path)
+
+		# 	stack.pop()
+			# actions.move_to_element(element).click().perform()
+
+	# except TimeoutException as e:
+	# 	element = -1
+	# 	print("TimeoutException",element.text)
+	# except WebDriverException:
+	# 	element = -1
+	# 	print("No futher pages",element.text)
+	# finally:
+	if len(stack) == 0:
+		print("return")
+		return driver, writer, stack, directory_path
+	else:
+		print("Finally, recurse")
+
+		# getFileStructure(element, driver, writer, stack, directory_path)
+		return driver, writer, stack, directory_path
+
+
+def check_stack(stack, directory_path):
+	return stack[-1]
+
+def stack_printing(stack, directory_path):
+	print('-------------------------------------------')
+	# print("The Stack: ", stack)
+	print('-------------------------------------------')
+	print("The readable: ", directory_path)
+	print('-------------------------------------------')
+
+def getFileList(element, driver, writer, stack, directory_path):
 	print('-------------------------------------------')
 	print("Get File List")
 	actions = ActionChains(driver)
@@ -133,25 +271,25 @@ def getFileList(element, driver, cookies, writer, stack, files_readable):
 	except WebDriverException:
 		element = -1
 		print("No Clicks: ",element)
-		return driver, cookies, writer, stack, files_readable
+		return driver, writer, stack, directory_path
 	
 	if element == -1: #No next page
 		elements = driver.find_elements(By.CLASS_NAME, "file-listing__name")
-		for first_out in files_readable[::-1]:
+		for first_out in directory_path[::-1]:
 			first_out_element = stack.pop()
 			if re.search(r'\.*', first_out):
-				value = files_readable.pop()
+				value = directory_path.pop()
 				writer.writerow([str(value)])
 			else:
 				#Go into the folder.
 				# element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "file-listing__item")))
 				actions.move_to_element(first_out_element).click().perform()
-				getFileStructure(element, driver, writer, stack, files_readable)
+				getFileStructure(element, driver, writer, stack, directory_path)
 
 		for i in elements[::-1]:
 			print(i)
 			actions.move_to_element(element).double_click().perform()
-			driver, cookies, writer, stack, files_readable = getFileList(element, driver, cookies, writer, stack, files_readable)
+			driver, writer, stack, directory_path = getFileList(element, driver, writer, stack, directory_path)
 
 			stack.pop()
 	else:
@@ -164,7 +302,7 @@ def getFileList(element, driver, cookies, writer, stack, files_readable):
 			element = -1
 			print("No futher pages",element.text)
 		finally:
-			return driver, cookies, writer, stack, files_readable
+			return driver, writer, stack, directory_path
 
 
 
@@ -198,11 +336,11 @@ def getFileList(element, driver, cookies, writer, stack, files_readable):
 	# 					writer.writerow(ele[0], "Checked")
 	# 					print("ele[0]: ", ele[0])
 	# 					actions.move_to_element(ele).double_click().perform #Click into the folder
-	# 					getFileStructure(element, driver, cookies, writer)
+	# 					getFileStructure(element, driver, writer)
 	# 					stack.pop()
 	# 				else:
 	# 					writer.writerow(ele[0])
-	# 	return driver, cookies, writer, stack, files_readable
+	# 	return driver, writer, stack, directory_path
 
 	# elif element != -1:
 	# 	driver.find_element(By.CLASS_NAME,"on-end")
@@ -213,10 +351,10 @@ def getFileList(element, driver, cookies, writer, stack, files_readable):
 	# 	print(current.text)
 	# 	for i in current.text:
 	# 		if str(i[0]) != "0":
-	# 			getFileList(element, driver, cookies, writer, stack, files_readable)
+	# 			getFileList(element, driver, writer, stack, directory_path)
 	# 		else:
 	# 			driver.find_element(By.CLASS_NAME,"file__back").click()
-	# 	getFileList(element, driver, cookies, writer, stack, files_readable)
+	# 	getFileList(element, driver, writer, stack, directory_path)
 
 
 	# if :
